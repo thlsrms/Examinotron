365c9ba554056535fbdaca50ca533d4f4c3fe2a6
Thales Ramos <thlsrmsdev@gmail.com>
2020-11-17 20:54:09 +0100

    fix en.DBNAME

--- db/mngdbconnect.js
+++ db/mngdbconnect.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const MONGODB_USER = process.env.MONGODB_USER;
 const MONGODB_PSSWRD = process.env.MONGODB_PSSWRD;
 const MONGODB_HOST = process.env.MONGODB_HOST;
-const MONGODB_DBNAME = process.env.MONGODB_PLAYLISTDB;
+const MONGODB_DBNAME = process.env.MONGODB_DBNAME;
 
 mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PSSWRD}@${MONGODB_HOST}/${MONGODB_DBNAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
